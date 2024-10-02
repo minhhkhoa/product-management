5ca8b97e5ecb88a2858fb5c023c7652eebcdade7
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-02 16:12:59 +0700

    4. Trang Dashboard bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,7 +1,50 @@
-//[get]: /admin/dashboard
+const ProductCategory = require("../../models/product-category.model")
 
-module.exports.dashboard = (req, res) => {
+//[get]: /admin/dashboard
+module.exports.dashboard = async (req, res) => {
+
+  //-thong ke
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+
+  //-update lai
+  statistic.categoryProduct.total = await ProductCategory.countDocuments({
+    deleted: false
+  })
+  statistic.categoryProduct.active = await ProductCategory.countDocuments({
+    deleted: false,
+    status: "active"
+  })
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    deleted: false,
+    status: "inactive"
+  })
+
+  //- tu lam tiep 3 tk con lai
+
+
   res.render("admin/pages/dashboard/index", {
-    pageTitle: 'Trang tổng quan'
+    pageTitle: 'Trang tổng quan',
+    statistic: statistic
   })
 }
