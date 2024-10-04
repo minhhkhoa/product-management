cb34d0ee7f9458e5f07b3b3d47542ff28ff40fe3
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-04 21:05:09 +0700

    1.1. Chèn icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // START_CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form")
 if (formSendData) { //-1
@@ -28,7 +30,7 @@ socket.on("SEVER_RETURN_MESSAGE", (data) => { //- nhan ve data da dc emit di tu 
     div.classList.add("inner-outgoing")
   } else {//- thang nhan phai dc ghi ten
     htmlFullName = `<div class="inner-name"> ${data.fullName}</div>`
-    div.classList.add("inner-incoming")
+    div.classList.add("inner-incoming") //- them ten class
   }
 
   div.innerHTML = `
@@ -44,7 +46,31 @@ socket.on("SEVER_RETURN_MESSAGE", (data) => { //- nhan ve data da dc emit di tu 
 
 //-start scroll chat to bottom
 const bodyChat = document.querySelector(".chat .inner-body")
-if(bodyChat){
+if (bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight
 }
 //-end scroll chat to bottom
+
+//- start show Icon chat
+//- start show popup
+const buttonIcon = document.querySelector('.button-icon')
+const tooltip = document.querySelector('.tooltip')
+Popper.createPopper(buttonIcon, tooltip)
+
+buttonIcon.onclick = () => {
+  tooltip.classList.toggle('shown')
+}
+//- end show popup
+
+//-start insert icon to input
+const emojiPicker = document.querySelector("emoji-picker")
+if (emojiPicker) {
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']")
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode
+    inputChat.value = inputChat.value + icon
+  })
+}
+//-end insert icon to input
+
+//- end show Icon chat
