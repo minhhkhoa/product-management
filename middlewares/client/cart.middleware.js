d805ce9b474ce6610f3400ac5900142111a08430
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-25 20:07:30 +0700

    Update cart.middleware.js

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,27 +1,36 @@
-const Cart = require("../../models/cart.model")
+const Cart = require("../../models/cart.model");
+const mongoose = require('mongoose');
 
 module.exports.cartId = async (req, res, next) => {
   if (!req.cookies.cartId) {
-    //-tao gio hang
-    const cart = new Cart()
-    await cart.save() //-tao gio hang moi trong db roi luu no vao cookies
+    // Tạo giỏ hàng mới
+    const cart = new Cart();
+    await cart.save(); // Lưu giỏ hàng mới vào DB
 
-    const expiresCookie = 365 * 24 * 60 * 60 * 1000 //- 1 giay
+    const expiresCookie = 365 * 24 * 60 * 60 * 1000; // Thời gian hết hạn cookie
 
     res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresCookie)
-    })
+    });
   } else {
-    //-lay ra gio hang thoi
-    const cart = await Cart.findOne({
-      _id: req.cookies.cartId
-    })
+    const cartId = req.cookies.cartId;
 
-    if (cart.products) { // co gio hang nhung chua co sp
-      cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
+    // Kiểm tra xem cartId có hợp lệ không
+    if (!mongoose.Types.ObjectId.isValid(cartId)) {
+      return res.status(400).send('Invalid Cart ID');
+    }
 
-      res.locals.miniCart = cart //miniCart = cart
+    // Lấy giỏ hàng
+    const cart = await Cart.findOne({ _id: cartId });
+
+    if (!cart) {
+      return res.status(404).send('Cart not found');
+    }
+
+    if (cart.products) { // Nếu có giỏ hàng nhưng chưa có sản phẩm
+      cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+      res.locals.miniCart = cart; // Gán cart vào res.locals
     }
   }
-  next()
-}
+  next();
+};
